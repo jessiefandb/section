9c4c4a0032deba7aef897397fd30f4878126659f
Jessie <jessie@fstln.io>
2024-09-11 18:17:58 +0800

    引入 tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["theme/sections/*.liquid", "theme/snippets/*.liquid"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
